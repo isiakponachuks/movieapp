f160a75bd33e602c457deb728d6f47fce68a6bb8
isiakponachuks <isi21001@byui.edu>
2022-06-05 06:38:10 +0100

    comit

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,23 +1,63 @@
-// wrapper for querySelector...returns matching element
-export function qs(selector, parent = document) {
-  return parent.querySelector(selector);
+export function getUrlParams(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const movie = urlParams.get(param);
+  return movie;
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
-// retrieve data from localstorage
-export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
+function convertToText(res) {
+  if (res.ok) {
+    return res.text();
+  } else {
+    throw new Error("Bad Response");
+  }
 }
-// save data to local storage
-export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  let clone = template.content.cloneNode(true);
+  if (callback) {
+    clone = callback(clone, data);
+  }
+  parentElement.appendChild(clone);
 }
-// set a listener for both touchend and click
-export function setClick(selector, callback) {
-  qs(selector).addEventListener("touchend", (event) => {
-    event.preventDefault();
-    callback();
-  });
-  qs(selector).addEventListener("click", callback);
+
+export async function loadTemplate(path) {
+  const html = await fetch(path).then(convertToText);
+  const template = document.createElement("template");
+  template.innerHTML = html;
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const currentPage = document.URL;
+  let initialPath = "";
+
+  if (
+    currentPage.includes("movie-details") ||
+    currentPage.includes("movie-listing")
+  ) {
+    initialPath = "../";
+  }
+
+  const header = await loadTemplate(`${initialPath}partials/header.html`);
+  const footer = await loadTemplate(`${initialPath}partials/footer.html`);
+  const headerElement = document.getElementById("main-header");
+  const footerElement = document.getElementById("main-footer");
+  renderWithTemplate(header, headerElement);
+  renderWithTemplate(footer, footerElement);
+}
+
+export function changeHeaderPath() {
+  const currentPage = document.URL;
+
+  if (
+    currentPage.includes("movie-details") ||
+    currentPage.includes("movie-listing")
+  ) {
+    const logoLink = document.querySelector("#logo-link");
+    const logoImg = document.querySelector("#logo-img");
+
+    logoLink.setAttribute("href", "../index.html");
+    logoImg.setAttribute("src", "../icons/8202294731595452646.svg");
+  }
 }
